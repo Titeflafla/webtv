23844d8f45c04d7d454dfd9f8135074f2f961991
Titeflafla <Titeflafla@users.noreply.github.com>
2013-12-06 06:15:08 +0100

    Update french.lang.php

--- modules/Web_tv/lang/french.lang.php
+++ modules/Web_tv/lang/french.lang.php
@@ -1,21 +1,21 @@
 <?php
 
-define("_NOENTRANCE","Désolé mais vous n'avez pas les droits pour accéder à cette page");
-define("_ZONEADMIN","Cette zone est réservée a l'Admin, Désolé...");
+define("_NOENTRANCE","DÃ©solÃ© mais vous n'avez pas les droits pour accÃ©der Ã  cette page");
+define("_ZONEADMIN","Cette zone est rÃ©servÃ©e a l'Admin, DÃ©solÃ©...");
 
 define("_ADMINWEBTV", "Admministration Web TV");
 define("_WEBTV", "Index");
-define("_ADDEVENT", "Ajouter un événement");
+define("_ADDEVENT", "Ajouter un Ã©vÃ©nement");
 define("_VOIRTV", "Voir les TV");
 define("_ADDTV", "Ajouter une TV");
-define("_SELECTTV", "Séléctionner une TV");
-define("_IMGGAME", "Séléctionner une image");
+define("_SELECTTV", "SÃ©lÃ©ctionner une TV");
+define("_IMGGAME", "SÃ©lÃ©ctionner une image");
 define("_TITLEGAME", "Nom du jeu");
-define("_SELECTJEUX", "Voir les icône");
+define("_SELECTJEUX", "Voir les icÃ´ne");
 define("_DE", "de");
-define("_ADDEVENT", "Ajouter l'événement");
-define("_MODIFEVENT", "Modifier l'événement");
-define("_NOEVENTWEBTV", "Rien de prévue !");
+define("_ADDEVENT", "Ajouter l'Ã©vÃ©nement");
+define("_MODIFEVENT", "Modifier l'Ã©vÃ©nement");
+define("_NOEVENTWEBTV", "Rien de prÃ©vue !");
 define("_URLTV", "Url de la TV");
 define("_WTV", "Largeur de la TV");
 define("_HTV", "Hauteur de la TV");
@@ -26,29 +26,31 @@ define("_ADDTV", "Ajouter une TV");
 define("_NOMTV", "Nom de la TV");
 define("_DESCR", "Description");
 define("_WEBTVID", "Sur la tv");
-define("_TVDELETE", "Web tv supprimé avec sucès !");
-define("_EVENTADD", "Evénement ajouté avec sucès !");
-define("_EVENTDELETE", "Evénement supprimé avec sucès !");
-define("_EVENTMODIF", "Evénement modifié avec sucès !");
-define("_NOTVBDD", "Aucune Web tv dans la base de donnée !");
-define("_NOEVENTBDD", "Il n'y a pas d'événement prévue !");
+define("_TVDELETE", "Web tv supprimÃ© avec sucÃ¨s !");
+define("_EVENTADD", "EvÃ©nement ajoutÃ© avec sucÃ¨s !");
+define("_EVENTDELETE", "EvÃ©nement supprimÃ© avec sucÃ¨s !");
+define("_EVENTMODIF", "EvÃ©nement modifiÃ© avec sucÃ¨s !");
+define("_NOTVBDD", "Aucune Web tv dans la base de donnÃ©e !");
+define("_NOEVENTBDD", "Il n'y a pas d'Ã©vÃ©nement prÃ©vue !");
 define("_IRCW", "Largeur du chat");
 define("_IRCH", "Hauteur du chat");
-define("_TVADD", "Web tv ajoutée avec sucès !");
+define("_TVADD", "Web tv ajoutÃ©e avec sucÃ¨s !");
 define("_MODIFTV", "Modifier cette TV");
-define("_TVMODIF", "Web tv modifiée avec sucès !");
+define("_TVMODIF", "Web tv modifiÃ©e avec sucÃ¨s !");
 define("_DESCRIPTIONTV", "Description de la TV");
 define("_AFFHISTORIQUE", "Afficher l'historique de la TV avec");
 define("_HISTORIQUEEVENTTV", "Historique de la TV");
 define("_NOEVENTBDDALLINDEX", "Aucun historique pour cette TV");
 define("_NOEVENTBDDTODAYINDEX", "Aucun historique pour aujourd'hui !");
-define("_NOEVENTBDDTODAYINDEXTV", "Aucun événement pour aujourd'hui !");
+define("_NOEVENTBDDTODAYINDEXTV", "Aucun Ã©vÃ©nement pour aujourd'hui !");
+define("_EDITTHISTV", "Editer cette TV");
+define("_DELTHISTV", "Supprimer cette TV");
 
-define("_ACTIONADDEVENT", "a ajouté l'événement :");
-define("_ACTIONMODIFEVENT", "a modifié l'événement :");
-define("_ACTIONDELEVENT", "a supprimé l'événement :");
-define("_ACTIONADDTV", "a ajouté la web tv :");
-define("_ACTIONMODIFTV", "a modifié la web tv :");
-define("_ACTIONDELTV", "a supprimé la web tv :");
+define("_ACTIONADDEVENT", "a ajoutÃ© l'Ã©vÃ©nement :");
+define("_ACTIONMODIFEVENT", "a modifiÃ© l'Ã©vÃ©nement :");
+define("_ACTIONDELEVENT", "a supprimÃ© l'Ã©vÃ©nement :");
+define("_ACTIONADDTV", "a ajoutÃ© la web tv :");
+define("_ACTIONMODIFTV", "a modifiÃ© la web tv :");
+define("_ACTIONDELTV", "a supprimÃ© la web tv :");
 
 ?>
